9b75bc1cd71b7554b114ffbb93a406df7d6cbc0f
Mohamed Nizar <nizarucsc@gmail.com>
2019-03-25 15:11:39 +0530

    Fix mysql ping

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,9 +33,9 @@ mysqldump \
   "${SRC_NAME}" \
   > /sql/dump.sql
 
-while ! mysqladmin ping -h "${DEST_HOST}" -u"${DEST_USER}" -p"${DEST_PASS}" --silent; do
+while ! mysqladmin --user="${DEST_USER}" --password="${DEST_PASS}"  --host "${DEST_HOST}"  ping --silent; do
     echo -e "MySQL server at ${DEST_HOST} not ready, trying again later..."
-    sleep 1
+    sleep 2
 done
 
 echo -e "Clearing destination database."
